416159c8127083b368c5d00ba40c4b81c093ace0
Mohamed Alshahoud <ay1665ay@gmail.com>
2025-03-26 15:32:38 +0100

    Ajout de données

---
+++ migrations/Version20250326142421.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250326142421 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE medecin (id INT AUTO_INCREMENT NOT NULL, spécialité_id INT DEFAULT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, INDEX IDX_1BDA53C6CE3C375B (spécialité_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE specialite (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE medecin ADD CONSTRAINT FK_1BDA53C6CE3C375B FOREIGN KEY (spécialité_id) REFERENCES specialite (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE medecin DROP FOREIGN KEY FK_1BDA53C6CE3C375B');
+        $this->addSql('DROP TABLE medecin');
+        $this->addSql('DROP TABLE specialite');
+    }
+}
