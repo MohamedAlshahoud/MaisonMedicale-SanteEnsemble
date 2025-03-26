76efca0b5041452b077df9e4f24cba02fe89de5a
Mohamed Alshahoud <ay1665ay@gmail.com>
2025-03-26 15:23:52 +0100

    Creation de l'entité Médecin

--- src/Entity/Specialite.php
+++ src/Entity/Specialite.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\SpecialiteRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -19,6 +21,17 @@ class Specialite
 
     #[ORM\Column(type: Types::TEXT)]
     private ?string $description = null;
+
+    /**
+     * @var Collection<int, Medecin>
+     */
+    #[ORM\OneToMany(targetEntity: Medecin::class, mappedBy: 'spécialité')]
+    private Collection $medecins;
+
+    public function __construct()
+    {
+        $this->medecins = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -45,6 +58,36 @@ class Specialite
     public function setDescription(string $description): static
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Medecin>
+     */
+    public function getMedecins(): Collection
+    {
+        return $this->medecins;
+    }
+
+    public function addMedecin(Medecin $medecin): static
+    {
+        if (!$this->medecins->contains($medecin)) {
+            $this->medecins->add($medecin);
+            $medecin->setSpécialité($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMedecin(Medecin $medecin): static
+    {
+        if ($this->medecins->removeElement($medecin)) {
+            // set the owning side to null (unless already changed)
+            if ($medecin->getSpécialité() === $this) {
+                $medecin->setSpécialité(null);
+            }
+        }
 
         return $this;
     }
