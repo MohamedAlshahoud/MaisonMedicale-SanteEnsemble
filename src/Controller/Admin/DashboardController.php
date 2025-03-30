d0c7a1f3781765c415b051b08b9b8966003cb381
Mohamed Alshahoud <ay1665ay@gmail.com>
2025-03-30 21:32:49 +0200

    Ajout du rôle Admin

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,42 @@
+<?php 
+
+namespace App\Controller\Admin;
+
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Medecin;
+use App\Entity\Patient;
+use App\Entity\RendezVous;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('ROLE_ADMIN')]
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin_dashboard')]
+    public function index(): Response
+    {
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        $url = $adminUrlGenerator->setController(PatientCrudController::class)->generateUrl();
+        return $this->redirect($url);
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Centre Médical - Admin');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        return [
+            MenuItem::linkToDashboard('Tableau de bord', 'fa fa-home'),
+            MenuItem::linkToCrud('Patients', 'fa fa-user', Patient::class),
+            MenuItem::linkToCrud('Médecins', 'fa fa-user-md', Medecin::class),
+            MenuItem::linkToCrud('Rendez-vous', 'fa fa-calendar-check', RendezVous::class),
+        ];
+    }
+}
