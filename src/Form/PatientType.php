e21da156cea92354400c42cd0e4d0a6c597c32ca
Mohamed Alshahoud <ay1665ay@gmail.com>
2025-03-31 21:53:03 +0200

    Création du formulaire d'inscription

---
+++ src/Form/PatientType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Patient;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PatientType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('nom', TextType::class, [
+            'label' => 'Nom'
+        ])
+        ->add('prenom', TextType::class, [
+            'label' => 'Prénom'
+        ])
+        ->add('email', EmailType::class, [
+            'label' => 'Email'
+        ])
+        ->add('password', PasswordType::class, [
+            'label' => 'Mot de passe'
+        ])
+        ->add('dateNaissance', DateType::class, [
+            'widget' => 'single_text',
+            'label' => 'Date de naissance'
+        ])
+        ->add('adresse', TextType::class, [
+            'label' => 'Adresse'
+        ])
+        ->add('telephone', TelType::class, [
+            'label' => 'Téléphone'
+        ])
+        ->add('submit', SubmitType::class, [
+            'label' => "S'inscrire"
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Patient::class,
+        ]);
+    }
+}
