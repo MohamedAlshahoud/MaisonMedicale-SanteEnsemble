7080ec94411eedd88137a2b491115d6c63ea35f3
Mohamed Alshahoud <ay1665ay@gmail.com>
2025-06-16 20:38:57 +0200

    Ajout des options personnalisées 'submit_label' et 'is_edit' dans PatientType

--- src/Form/PatientType.php
+++ src/Form/PatientType.php
@@ -4,13 +4,9 @@ namespace App\Form;
 
 use App\Entity\Patient;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TelType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -32,13 +28,17 @@ class PatientType extends AbstractType
             ->add('dateNaissance')
             ->add('adresse')
             ->add('telephone')
-            ->add('submit', SubmitType::class, ['label' => "S'inscrire"]);
+            ->add('submit', SubmitType::class, [
+                'label' => $options['submit_label']
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => Patient::class,
+            'is_edit' => false,
+            'submit_label' => "S'inscrire",
         ]);
     }
 }
