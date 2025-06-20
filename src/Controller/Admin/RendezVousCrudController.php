f36af7cd9382f6ce1646f3430e03b961eb89fecc
Mohamed Alshahoud <ay1665ay@gmail.com>
2025-06-20 21:03:23 +0200

    Correction des champs EasyAdmin pour l'entité RendezVous

--- src/Controller/Admin/RendezVousCrudController.php
+++ src/Controller/Admin/RendezVousCrudController.php
@@ -7,6 +7,8 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -31,8 +33,9 @@ class RendezVousCrudController extends AbstractCrudController
     {
         return [
             IdField::new('id')->hideOnForm(),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            AssociationField::new('medecin'),
+            AssociationField::new('patient'),
+            DateTimeField::new('dateHeure'),
         ];
     }
 
